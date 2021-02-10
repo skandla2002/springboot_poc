ad090dc935fb5815b338fa9e5efdd7cdab1bccba
Andy Pak <skandla2002@gmail.com>
2021-02-11 00:42:43 +0900

    글 목록 화면 개발 - update: 글 목록 화면 생성

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,10 +1,16 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
 // ibatis, MyNBatis의 Dao와 같은 DB Layer 접근자임
 // JpaRepository<Entity 클래스, PK타입>을 상속하면 기본 CRUD 메소드가 자동 생성
 // 단, Entity클래스와 Repository는 함께 위치해야함
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts>  findAllDesc();
 }

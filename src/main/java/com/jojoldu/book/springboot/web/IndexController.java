e046a0abe5e268f2d3f911ba48edaaa302ca3927
Andy Pak <skandla2002@gmail.com>
2021-02-11 12:36:51 +0900

    글 목록 화면 개발 - update: 글 목록 화면 서버 RENDER 추가(Model 전달)

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,18 +1,26 @@
 package com.jojoldu.book.springboot.web;
 
 
+import com.jojoldu.book.springboot.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){ // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장(결과를 index.mustache에 전달)
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
+//    public String index(){
+//        return "index";
+//    }
+
 
     @GetMapping("/posts/save")
     public String postsSave(){

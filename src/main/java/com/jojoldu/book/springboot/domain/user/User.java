4dcc641b9999f520ccdc943dea2a61c4f1b01472
Andy Pak <skandla2002@gmail.com>
2021-02-13 04:39:34 +0900

    User Class 생성

---
+++ src/main/java/com/jojoldu/book/springboot/domain/user/User.java
@@ -0,0 +1,51 @@
+package com.jojoldu.book.springboot.domain.user;
+
+
+import com.jojoldu.book.springboot.domain.posts.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING) // JPA로 데이터 베이스 저장시 Enm 값을 어떤 형태로 저장할지 결정함
+                                 // (기본 int 임, 숫자 저장시 DB로 확인시 어떤 코드 의미인지 알 수 없음)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role){
+        this.name = name;
+        this.email = email;
+        this.picture = pictrue;
+        this.role = role;
+    }
+
+    public User update(String name, String picture){
+        this.name = name;
+        this.picture = pictrure;
+        return this;
+    }
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+}

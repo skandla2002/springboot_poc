fd8cd983d97cf16b071fc926853c560ec1d2c7ac
Andy Pak <skandla2002@gmail.com>
2021-02-15 22:49:42 +0900

    @LoginUser 세션 유지 기능 추가

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,6 +1,8 @@
 package com.jojoldu.book.springboot.web;
 
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -14,9 +16,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postsService;
-    @GetMapping("/")
-    public String index(Model model){ // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장(결과를 index.mustache에 전달)
+    @GetMapping("/") // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장(결과를 index.mustache에 전달)
+    // 기존 (User) httpSession.getAttribute("user")로 가져오던 세션 정보 값이 개선됨, 어디서든 @LoginUser로 세션정보 가져 올 수 있음
+    public String index(Model model, @LoginUser SessionUser user){ 
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 //    public String index(){
